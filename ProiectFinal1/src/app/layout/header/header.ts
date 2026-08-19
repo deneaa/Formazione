6524aeb6e42db89dbd2b2f16ec95a7a5e2c88b4d
Denis <denis.stefanw@gmail.com>
2026-08-19 11:33:24 +0300

    Fix Layout

--- ProiectFinal1/src/app/layout/header/header.ts
+++ ProiectFinal1/src/app/layout/header/header.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LucideUser } from '@lucide/angular';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, LucideUser],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
--- ProiectFinal1/src/app/layout/sidenav/sidenav.ts
+++ ProiectFinal1/src/app/layout/sidenav/sidenav.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideHouse, LucideShoppingBag, LucideShoppingCart, LucideUser, LucideSettings } from '@lucide/angular';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LucideHouse, LucideShoppingBag, LucideShoppingCart, LucideUser, LucideSettings],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })

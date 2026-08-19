4917754aa92f08c69a8c644a6e12d4a350d7931a
Denis <denis.stefanw@gmail.com>
2026-08-19 11:07:36 +0300

    Add Pages and Routes

--- ProiectFinal1/src/app/app.routes.ts
+++ ProiectFinal1/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Profile } from './pages/profile/profile';
+import { Cart } from './pages/cart/cart';
+import { Admin } from './pages/admin/admin';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'products/:id', component: ProductDetail },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: 'cart', component: Cart, canActivate: [authGuard] },
+  { path: 'admin', component: Admin, canActivate: [authGuard], data: { role: 'Admin' } },
+  { path: '**', redirectTo: '' },
+];

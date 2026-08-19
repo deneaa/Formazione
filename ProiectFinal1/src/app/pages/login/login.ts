304bdd7b71d882567ac664a6b3099d440ccb6244
Denis <denis.stefanw@gmail.com>
2026-08-19 12:20:41 +0300

    Add Login and Register Page

--- ProiectFinal1/src/app/pages/login/login.ts
+++ ProiectFinal1/src/app/pages/login/login.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  errorMessage = '';
+
+  form = this.fb.nonNullable.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const result = this.auth.login(this.form.getRawValue());
+
+    if (!result.success) {
+      this.errorMessage = result.message ?? 'Eroare la login.';
+      return;
+    }
+
+    this.router.navigate(['/']);
+  }
+}
--- ProiectFinal1/src/app/pages/register/register.ts
+++ ProiectFinal1/src/app/pages/register/register.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  private fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  errorMessage = '';
+
+  form = this.fb.nonNullable.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const result = this.auth.register(this.form.getRawValue());
+
+    if (!result.success) {
+      this.errorMessage = result.message ?? 'Eroare la înregistrare.';
+      return;
+    }
+
+    this.router.navigate(['/']);
+  }
+}

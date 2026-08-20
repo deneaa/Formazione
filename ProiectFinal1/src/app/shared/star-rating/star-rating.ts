c22477bca13206dd8ad82ed4508f9613f1b851e2
Denis <denis.stefanw@gmail.com>
2026-08-20 11:47:31 +0300

    Solved Stars

--- ProiectFinal1/src/app/shared/star-rating/star-rating.ts
+++ ProiectFinal1/src/app/shared/star-rating/star-rating.ts
@@ -1,8 +1,9 @@
 import { Component, input, output } from '@angular/core';
+import { LucideStar } from '@lucide/angular';
 
 @Component({
   selector: 'app-star-rating',
-  imports: [],
+  imports: [LucideStar],
   templateUrl: './star-rating.html',
   styleUrl: './star-rating.css',
 })
@@ -10,12 +11,18 @@ export class StarRating {
   value = input<number>(0);
   interactive = input<boolean>(false);
   rated = output<number>();
-
   stars = [1, 2, 3, 4, 5];
 
   onClick(star: number): void {
     if (this.interactive()) {
       this.rated.emit(star);
     }
+  }
+
+  fillPercent(star: number): number {
+    const diff = this.value() - (star - 1);
+    if (diff >= 1) return 100;
+    if (diff >= 0.5) return 50;
+    return 0;
   }
 }
